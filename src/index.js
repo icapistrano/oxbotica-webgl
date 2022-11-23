d22a45d2d413987a8543b21809add7b7be64f181
Immanuel Capistrano <icapistrano97@outlook.com>
2022-11-23 11:54:26 +0000

    webpack setup, fix three dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 import { SceneManager } from './components/SceneManager.js';
 import { MapHandler } from './components/MapHandler.js';
 import { Marker } from './components/Marker.js';
